849c64ba5473affd49a8c8d99ba3ea7f02610509
fengmin <fengmin@koolearn.com>
2021-01-25 17:03:44 +0800

    fdf

---
+++ git_commit_pre_hookTests/git_commit_pre_hookTests.m
@@ -0,0 +1,36 @@
+//
+//  git_commit_pre_hookTests.m
+//  git_commit_pre_hookTests
+//
+//  Created by fengmin on 2021/1/25.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface git_commit_pre_hookTests : XCTestCase
+
+@end
+
+@implementation git_commit_pre_hookTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
